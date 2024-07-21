aca983c5e15700ad0956662b085643ae2e9a0fef
Rajan <rajanj448@student.sfit.ac.in>
2024-07-21 14:54:28 +0530

    initial

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="flex w-screen overflow-hidden overscroll-none flex-col h-screen bg-background">
+      <div className="h-12 flex">Navbar</div>
+      <div className="w-screen flex grow">
+        
+      </div>
+    </div>
+  );
+}
