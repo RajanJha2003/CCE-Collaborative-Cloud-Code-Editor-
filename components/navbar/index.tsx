8af3d603c342f12e52bc1767fd7e721dbc974767
Rajan <rajanj448@student.sfit.ac.in>
2024-07-22 12:16:44 +0530

    Added Search Component

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -6,25 +6,25 @@ import Image from "next/image";
 import { UserButton } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import Link from "next/link";
+import DashboardNavbarSearch from "./search";
 
 const Navbar = () => {
   return (
     <div className="h-14 px-2 w-full border-b border-border flex items-center justify-between">
-      <div className="flex items-center space-x-4">
-        <Link href={"/"} className="ring-offset-2 ring-offset-background focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none rounded-sm">
-          <Image src={Logo} alt="Logo" width={36} height={36} />
-        </Link>
-        <div className="flex items-center text-sm font-medium">
-          My React Project
-          <div className="h-7 w-7 flex ml-1 items-center justify-center transition-colors bg-transparent hover:bg-muted-foreground/25 rounded-md ">
-            <Pencil className="h-4 w-4" />
-          </div>
-        </div>
-      </div>
-      <UserButton appearance={{
-        baseTheme:dark
-      }} afterSignOutUrl="/" />
+    <div className="flex items-center space-x-4">
+      <Link
+        href={"/"}
+        className="ring-offset-2 ring-offset-background focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none rounded-sm"
+      >
+        <Image src={Logo} alt="Logo" width={36} height={36} />
+      </Link>
+      <div className="text-sm font-medium flex items-center">Virtualbox</div>
     </div>
+    <div className="flex items-center space-x-4">
+      <DashboardNavbarSearch />
+      <UserButton  />
+    </div>
+  </div>
   );
 };
 
